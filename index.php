ac743b8d5819b42b3bf7b5d7409d0be468217e3e
Frank <fudaofeng@qq.com>
2017-09-11 15:30:59 +0800

    init

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<script src="//apps.bdimg.com/libs/jquery/2.1.4/jquery.js"></script>
+<title>CVMing</title>
+<style>
+    body{
+        background: black none repeat scroll 0% 0%; 
+        color: peru;
+    }
+</style>
+</head>
+<body>
+ <h1>Hi...</h1>
+</body>
+</html>
+
