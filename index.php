a85c62eafec3451c0afebd893c549b9c1a532d42
Frank <fudaofeng@qq.com>
2017-09-12 15:04:11 +0800

    init

--- index.php
+++ index.php
@@ -3,16 +3,20 @@
 <head>
 <meta charset="utf-8">
 <script src="//apps.bdimg.com/libs/jquery/2.1.4/jquery.js"></script>
+<link type="text/css" rel="stylesheet" href="style.css" />
 <title>CVMing</title>
-<style>
-    body{
-        background: black none repeat scroll 0% 0%; 
-        color: peru;
-    }
-</style>
 </head>
 <body>
- <h1>Hi...</h1>
+<div id="container">
+    <div id="header">
+      header
+    </div>
+    <div id="content">
+      <div class="left-menu">left menu</div>
+      <div class="main-content"> main content</div>
+    </div>
+    <div id="footer">footer</div>
+
+</div>
 </body>
 </html>
-
