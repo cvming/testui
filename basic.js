dbc50d97f8ee292d6fbd7155ed16713a8cb57d21
Frank <fudaofeng@qq.com>
2017-09-12 16:05:39 +0800

    add js

---
+++ basic.js
@@ -0,0 +1,3 @@
+$(document).ready(function(){
+    alert('System init, waiting...');
+});
--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <head>
 <meta charset="utf-8">
 <script src="//apps.bdimg.com/libs/jquery/2.1.4/jquery.js"></script>
+<script src="basic.js"></script>
 <link type="text/css" rel="stylesheet" href="style.css" />
 <title>CVMing</title>
 </head>
